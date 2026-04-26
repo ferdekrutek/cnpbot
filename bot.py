7fea801ba0ec432191b7eb957e011a4e7e3e7dbc
ferdekrutek <ferdek.rutek@gmail.com>
2026-04-26 15:58:31 +0200

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,7 +7,10 @@ import os
 # ─────────────────────────────────────────────
 #  Konfiguracja
 # ─────────────────────────────────────────────
-TOKEN = "MTQ5NzkzOTExNzcyMjQzNTYxNA.GR77mS.zz5ExhEMgDCKTZSeEBg6tK9AR5FY-TZgKnISFY"
+TOKEN = os.getenv("DISCORD_TOKEN")
+if not TOKEN:
+    raise ValueError("Brak tokenu DISCORD_TOKEN!")
+print(f"Token załadowany, długość: {len(TOKEN)}")
 
 intents = discord.Intents.default()
 intents.members = True          # wymagane do pobierania członków serwera
